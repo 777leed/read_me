20f3f9516b4e8213612255c6a23367043b537fb0
Khalid koukou <86270022+777leed@users.noreply.github.com>
2023-09-02 00:02:53 +0100

    messed up

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
 <div class="app">
+
+<div class="holder">
+
+
 <ul class="cards">
   <li class="cards__item">
     <div class="card">
@@ -7,7 +11,7 @@
       <div class="card__content">
         <div class="card__title">The land of the dead</div>
         <p class="card__text">His father was a monster, the kind one, the one you find in all of us. The ashes of his mother are all over the place now.</p>
-        <button class="btn btn--block card__btn"><a href="land_of_the_dead.html">Read More</a></button>
+        <button class="btn btn--block card__btn"><a href="#t">Read More</a></button>
       </div>
     </div>
   </li>
@@ -17,7 +21,7 @@
       <div class="card__content">
         <div class="card__title">4:33 AM</div>
         <p class="card__text">His coffee was already cold when he got up, 4:33 she left the bed again without a word ,I can't remember the last time I saw her face, I haven't seen her lips move in so long,</p>
-        <button class="btn btn--block card__btn"><a href="433.html">Read More</a></button>
+        <button class="btn btn--block card__btn"><a href="#4">Read More</a></button>
       </div>
     </div>
   </li>
@@ -27,11 +31,66 @@
       <div class="card__content">
         <div class="card__title">Rosemary District</div>
         <p class="card__text">It was a dark night as usual, the broken moon shinning its dim light as if it was trying so hard to guide him through these dead-silent hallways</p>
-        <button class="btn btn--block card__btn"><a href="rosemary.html">Read More</a></button>
+        <button class="btn btn--block card__btn"><a href="#r">Read More</a></button>
       </div>
     </div>
   </li>
 </ul>
+
+</div>
+
+
+<header id="t">
+  <h1>The Land of The Dead</h1>
+</header>
+<main>
+  <p>
+      His father was a monster, the kind one, the one you find in all of us. The ashes of his mother are all over the place now. Reviving the dead is a sin up in here in Syrex, but what sin is greater than forgetting your loved ones.
+
+      The reviving didn't work this time again and so is everything in Leann's life, "Only failures" he said with the last sparkle of hope shining in his tired eyes.
+      
+      Of course he's going to try again and certainly he's going to fail again.
+      
+      He put his vaporized mother back in the pot and left the house for some fresh air, or that's what he convinced himself he was doing, there is barely any air left in Syrex.
+      
+      He put his breathing mask on his wrinkly face, his black leather gloves on his pale old-looking hands and perceived to evacuate the building, It's on fire again, the third time this week.
+      
+      Unlike in the previous planet the fire flames in Syrex are pink but harmless they just burst out of nowhere and the weathercast is not that accurate.
+      
+      Leann is used to it now, he is out from the burning place, "they're going to put it off again, and light it again and put it off again and ligh-...".
+      
+      Speaking to oneself is what Leann does best since he lived alone for most of his "said" life.
+      
+      Leann is turning 23 this month, not very excited he thinks he lived enough in his previous life so why repeat another one.
+      
+      Yara his dead wife was the reason he is still living.
+      
+      Living for dead people is just something he's doing at this point of his life. "it was an accident" he murmured while walking down the street remembering her again even though her picture in his mind is getting blurrier by the day, but he still remembers her smile and how she laughed so casually. A salty liquid drops and slides across his face, tears or rain ?, he couldn't care less. his face might look tough but the some of tears and cries he had are enough to fill up a lake and bring down a mountain to the ground but none of those can bring Yara back, or his mother for that matter.
+      
+      It's been months since Leann spoke to his father, He doesn't really miss him anyways, like every son-dad relationship in Syrex he says hey every once in a year, sometimes even years. Time passes slowly in here which only makes things more painful for everyone , you can feel the heaviness of time and gravity is pulling people down to the ground as if it was calling them to an eternal sleep. You can't really die in this planet, it was made like that, you either choose eternal suffering or eternal happiness , well of course some people choose both, But Leann didn't choose any of them he is just breathing what's left of his heavy oxygen tank. The only choice he made was to come to Syrex which is a dumb choice, A choice he regretted after a short time on arriving, but then that regret turned down Its flames and Leann doesn't really care anymore, No miracle can save him now and all of his luck ended when he stepped foot into that portal to the land of the dead "Syrex".
+  </p>
+</main>
+
+<header id="4">
+  <h1>4:33 AM</h1>
+</header>
+<main>
+  <p>
+      His coffee was already cold when he got up, 4:33 she left the bed again without a word, I can't remember the last time I saw her face, I haven't seen her lips move in so long, what is she thinking leaving me voicemails every morning like I'm living with some ghost. Your wife is dead; she always has been. What are you still waiting for? It's a cold night, and you are staying up late again. Do you want to get sick again? Don't you remember what your doctor said? She opened the drawer, made him swallow his pills, now he is asleep.<br><br>
+      He looks so peaceful when he is asleep. Isn't it wonderful this temporal death we have been blessed with? All the dead people are asleep, and all the asleep ones are dead, both lifeless and can do no harm. No prison is tighter than one's mind, and nothing is more precious than someone's life. Who could've believed that this sleeping angel took her life so vainly? He was never stable; we always knew he could do something regretful. Regret found no way to slip through his wrecked mind, neither does his wife; she's still there trapped within topless walls and bottomless pits...
+  </p>
+</main>
+
+<header id="r">
+  <h1>Rosemary District</h1>
+</header>
+<main>
+  <p>
+      It was a dark night as usual, the broken moon shinning its dim light as if it was trying so hard to guide him through these dead-silent hallways, people were dying in the corners of every street without much of a noise.no one would care even if they screamed their lungs out. It's a very selfish world and there is no one to blame for that.
+      The mayor of Rosemary district was unusually overweight you can hear the grease boiling in his greedy stomach if you get close enough, just like every needy mayor of any region they only fear two things : losing their wealth, and the supreme ruler of this god-awful empire Eris, She took the throne after her father passed away, rumors say she killed him herself but she was so fearsome no soul could dear to oppose her eye gaze. campaigns to execute the opposing party were all she have done since her ceremony of crowning.
+  </p>
+</main>
+
 </div>
 </template>
 
@@ -49,12 +108,53 @@ export default {
   box-sizing: border-box;
 }
 
+.holder {
+  min-height: 100vh;
+  display: flexbox;
+  justify-content: center;
+  align-items: center;
+
+}
+
 a {
   text-decoration: none;
   color: #696969;
 }
 html {
   background-color: #f5f5f5;
+}
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+  color: #333;
+  line-height: 1.6;
+}
+
+header {
+  background-color: #09131f;
+  color: #fff;
+  text-align: center;
+  padding: 20px 0;
+}
+
+h1 {
+  font-size: 36px;
+  margin: 0;
+}
+
+main {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: justify;
+}
+
+p {
+  font-size: 16px;
+  margin: 0 0 20px;
+  color: gray;
 }
 body {
   color: #999999;
